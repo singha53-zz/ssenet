1aaa8a7c98d4ade3aa58ac1ee43d34c574a42d2a
Amrit Singh <asingh.analytics@gmail.com>
2020-03-09 23:13:02 -0400

    fix predict class

--- R/predict.R
+++ R/predict.R
@@ -1,7 +1,7 @@
 #' cross-validation function for all methods
 #'
 #' Estimate test error of elastic net panel
-#' @param x an object of a specific machine learning method (ML) class
+#' @param object an object of a specific machine learning method (ML) class
 #' @param validation a string type of validation; either "Mfold" or "loo"
 #' @param M an integer specifying the number of folds
 #' @param iter an integer specifying the number of times to repeat the cross-validation
@@ -9,5 +9,5 @@
 #' @param progressBar a boolean that specifies whether a progress bar should be displayed or not
 #' @export
 predict <- function(object, ...){
-  UseMethod(generic = "predict", object = object)
+  UseMethod("predict")
 }
